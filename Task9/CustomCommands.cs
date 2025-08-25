174546b820e8c3685e417fd103d88a80d4cf4e7e
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-08-25 21:47:47 +0300

    Task9

---
+++ Task9/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Task9
+{
+    internal class CustomCommands
+    {
+        public static readonly RoutedUICommand ChangeColor = new RoutedUICommand(
+            "Изменить цвет",
+            "ChangeColor",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.C, ModifierKeys.Control)
+            });
+    }
+}
---
+++ Task9/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Stack<Brush> _colorHistory = new Stack<Brush>();
+
+        private Brush _currentColor;
+
+        // Текущий цвет
+        public Brush CurrentColor
+        {
+            get => _currentColor;
+            set
+            {
+                _currentColor = value;
+
+                // Добавляем новый цвет в стек, если предыдущий цвет не такой же
+                // (либо стек пустой) 
+                if (_colorHistory.Count == 0 || _colorHistory.Peek() != value)
+                {
+                    _colorHistory.Push(value);
+                }
+                // Dock - это имя (x:Name) контейнера компоновки, для которого меняем цвет
+                Dock.Background = _currentColor;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var random = new Random();
+            // CurrentColor - это свойство для хранения текущего цвета
+            // ниже описано, как оно устроено
+            CurrentColor = new SolidColorBrush(Color.FromRgb(
+                (byte)random.Next(256),
+                (byte)random.Next(256),
+                (byte)random.Next(256)));
+        }
+        private void UndoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_colorHistory.Count > 1)
+            {
+                _colorHistory.Pop(); // Удаляем текущий цвет
+                CurrentColor = _colorHistory.Pop(); // Берём предыдущий
+            }
+        }
+        private void UndoCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _colorHistory.Count > 1;
+        }
+    }
+}
